acfdaa36158918e9c160dd5b228599cf1a6f860a
RAMESH MARANDI <57364523+rameshmarandi@users.noreply.github.com>
2026-02-06 12:25:18 +0530

    removed

--- components/Header.js
+++ components/Header.js
@@ -32,7 +32,7 @@ export default function Header() {
             href="/"
             className="text-xl font-bold shrink-0"
             style={{ color: "var(--primary)" }}>
-            Swiftcalcfy(Ramesh Marandi Sanda sdf)
+            Swiftcalcfy
           </Link>
 
           {/* SEARCH */}
